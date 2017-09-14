02abcc208e740ab338fa7ad31a2323a9c71b7336
Thomas <tseabolt19@gmail.com>
2017-09-14 10:02:58 -0500

    initial commit

---
+++ lab1_a.c
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : lab1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+#define pi 3.14 //define pi
+
+int main(void) {
+	float radius = 0.0;//initialize variables
+	float area = 0.0;
+	printf("What is the radius of your circle?");
+	scanf("%f", &radius);//get radius of circle
+	if(radius >= 0) {
+		area = pi * pow(radius, 2); //calculate area
+		printf("The area of your circle is %f", area);
+	} else { //if radius entered is < 0
+		printf("Radius cannot be less than zero.");
+	}
+	return 0;
+}
